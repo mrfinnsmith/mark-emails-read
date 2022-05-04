3600acf449558b89702697e9feebb502b8d6c59f
Finn Smith <finnbarsmith@gmail.com>
2022-05-04 12:50:00 -0700

    Fix typo in searchAndMarkRead - it had an extra e

--- markEmailsRead.js
+++ markEmailsRead.js
@@ -1,14 +1,17 @@
 /*
 What this is:
+Values of targetLabelNames and oldEmailDefinition are configurable in config.js/gs. 
 
-labelName variable at the top of the file can be configured to look for any other label name. Gmail nested labels are named with the parent label, then "/", then the child label. So if you make labelName 'newsletter,' this script will find any email labeled 'newsletter,' as well as anything labeled 'newsletter/fun' or 'newsletter/local.'
+Gmail nested labels are named with the parent label, then "/", then the child label. So if you make one of your targetLabelNames 'newsletter,' this script will find any email labeled 'newsletter,' as well as anything labeled 'newsletter/fun' or 'newsletter/local.'
  */
 
+
+// main gets the target labels and the number of days to look at from config.js/gs, then runs the function to searchAndMarkRead.
 function main() {
-  config.targetLabelNames.forEach(targetLabelName => {
+  config.targetLabelNames.forEach(targetLabelName => { // Value of targetLabelNames is configurable in config.js/gs
 
     let d = new Date();
-    d.setDate(d.getDate() - 1);
+    d.setDate(d.getDate() - config.oldEmailDefinition); // Value of oldEmailDefinition is configurable in config.js/gs
 
 
     let dateYear = d.getFullYear();
@@ -19,7 +22,7 @@ function main() {
 
     targetLabels = findLabels(targetLabelName);
 
-    searchAndMarkeRead(targetLabels, fullDate);
+    searchAndMarkRead(targetLabels, fullDate);
   }
   );
 }
@@ -37,7 +40,7 @@ function findLabels(targetLabel) { // This function grabs an array of labels tha
   return labelArray;
 }
 
-function searchAndMarkeRead(searchLabels, searchDate) {
+function searchAndMarkRead(searchLabels, searchDate) {
   for (i in searchLabels) {
     let thisLabelName = searchLabels[i];
     let query = 'label:' + thisLabelName + ' is:unread before:' + searchDate;
